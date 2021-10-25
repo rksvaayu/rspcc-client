1961fc65e438e4d992e7339ab298d8caa75604ca
Rohit Kumar <sangal.rohit@gmail.com>
2021-10-25 11:27:01 -0400

    added public
    added public

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-export default axios.create({
+/* export default axios.create({
   baseURL: "http://localhost:8080/api",
+    headers: {
+    "Content-type": "application/json"
+  }
+}); */
+
+export default axios.create({
+  baseURL: "http://10.60.19.38:8081/api",
   headers: {
     "Content-type": "application/json"
   }
